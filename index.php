b345f6bb9af2b0d77f1582728fca5129cc06a8c7
Happykiller <fabrice.rosito@gmail.com>
2015-04-30 12:24:47 +0200

    no message

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 // API/phpsql/exemple.php?milis=123450&ctrl=ok&param_name=nom_site
 require "vendor/autoload.php";
 
+/**
+ * CONFIG PART
+ */
 $config = \Oda\SimpleObject\OdaConfig::getInstance();
 $config->domaine = "http://localhost/ODA_SANDBOX/";
 
@@ -10,10 +13,17 @@ $config->BD_ENGINE->base = 'how';
 $config->BD_ENGINE->mdp = 'pass';
 $config->BD_ENGINE->prefixTable = 'how-';
 
+/**
+ * BUILD INTERFACE
+ */
 $params = new \Oda\SimpleObject\OdaPrepareInterface();
 $params->arrayInput = array("param_name");
 $ODA_INTERFACE = new \Oda\OdaLibInterface($params);
 
+/**
+ * USE BD
+ */
+//Build query
 $params = new \Oda\SimpleObject\OdaPrepareReqSql();
 $params->sql = "SELECT *
     FROM `api_tab_parametres` a
@@ -26,6 +36,7 @@ $params->bindsValue = [
 $params->typeSQL = \Oda\OdaLibBd::SQL_GET_ONE;
 $retour = $ODA_INTERFACE->BD_ENGINE->reqODASQL($params);
 
+//Add result to interface
 $params = new \stdClass();
 $params->label = "resultat_get_one";
 $params->retourSql = $retour;
