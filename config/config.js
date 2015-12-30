663f3b435daa15ecec9d892307ab6d90ba91f8c6
Rosito Fabrice <fabrice.rosito@gmail.com>
2015-12-30 01:11:54 +0100

    update

--- config/config.js
+++
@@ -1,4 +0,0 @@
-$.Oda.Context.host = "http://localhost/chop/";
-$.Oda.Context.rest = "http://localhost/chop/server/";
-$.Oda.Context.projectLabel = "chop";
-$.Oda.Context.resources = "http://localhost/chop/server/resources/";
